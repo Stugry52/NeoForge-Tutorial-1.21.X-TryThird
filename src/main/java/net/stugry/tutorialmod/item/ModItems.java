8fa7af542a3037d1cbc6907aa38faa5a15201a4b
Stugry52 <vladkravcenko95@gmail.com>
2026-05-06 21:36:49 +0300

    items_is_work

--- src/main/java/net/stugry/tutorialmod/item/ModItems.java
+++ src/main/java/net/stugry/tutorialmod/item/ModItems.java
@@ -1,5 +1,8 @@
 package net.stugry.tutorialmod.item;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -10,7 +13,12 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MOD_ID);
 
     public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().setId(
+                    ResourceKey.create(
+                            Registries.ITEM,
+                            ResourceLocation.fromNamespaceAndPath(TutorialMod.MOD_ID, "bismuth")
+                    )
+            )));
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);

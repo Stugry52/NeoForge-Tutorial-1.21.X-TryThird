244ae1e1fc01bfc79cc7cefb955438edadbe8906
Stugry52 <vladkravcenko95@gmail.com>
2026-05-08 18:53:32 +0300

    CreativeTab

--- src/main/java/net/stugry/tutorialmod/TutorialMod.java
+++ src/main/java/net/stugry/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.stugry.tutorialmod;
 
 import net.stugry.tutorialmod.block.ModBlocks;
+import net.stugry.tutorialmod.item.ModCreativeModeTabs;
 import net.stugry.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -49,6 +50,8 @@ public class TutorialMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);

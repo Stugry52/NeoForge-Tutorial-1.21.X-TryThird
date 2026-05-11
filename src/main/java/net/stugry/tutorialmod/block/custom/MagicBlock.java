bb04a56f9c054e306e5e9d58a37815128052c018
Stugry52 <vladkravcenko95@gmail.com>
2026-05-11 16:24:01 +0300

    Tags

--- src/main/java/net/stugry/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/stugry/tutorialmod/block/custom/MagicBlock.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.stugry.tutorialmod.item.ModItems;
+import net.stugry.tutorialmod.util.ModTags;
 
 public class MagicBlock extends Block {
 
@@ -33,7 +34,8 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof ItemEntity itemEntity){
-            if (itemEntity.getItem().getItem() == ModItems.RAW_BISMUTH.get()){
+
+            if (isValidItem(itemEntity.getItem())){
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
             if (itemEntity.getItem().getItem() == Items.DANDELION){
@@ -43,6 +45,10 @@ public class MagicBlock extends Block {
 
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
 }
